bdb4fa1f429371d5624eff458eee453797c11177
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-11-01 17:02:13 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,3 +95,20 @@ options = st.multiselect(
      ['Yellow', 'Red'])
 
 st.write('You selected:', options)
+
+st.header('st.checkbox')
+
+st.write ('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
